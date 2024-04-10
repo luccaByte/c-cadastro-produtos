c0de5bd97123df9700354b3b158f6a91023e8b66
lucas <lucaston2323@gmail.com>
2024-04-10 19:25:20 -0300

    fix: correção de algumas funções

--- Cadastro de Produtos/Cadastro de Produtos.cpp
+++ Cadastro de Produtos/Cadastro de Produtos.cpp
@@ -1,105 +1,127 @@
 #include <cstdio>
+#include <cfloat>
 #include <string>
 #pragma warning(disable : 4996)
 
-typedef struct PerfilProduto
+typedef struct PerfilProduto 
 {
-	char nomeProduto[100];
-	char marca[50];
-	char quantidade[25];
-	float preco[3];
+    char nomeProduto[100];
+    char marca[50];
+    char quantidade[25];
+    float preco[3];
 };
 
 PerfilProduto produtos[10];
-int produtosCadastrados = 1;
+// contador
+int produtosCadastrados = 0;
 
-void inicializacao()
+void inicializacao() {
+    produtosCadastrados = 0;
+}
+
+void cadastroProdutos() 
 {
-	produtosCadastrados = 0;
-};
+    if (produtosCadastrados < 10) 
+    {
+        printf("\n***************************************************\n");
+        printf("Digite o nome do produto: ");
+        scanf("%s", produtos[produtosCadastrados].nomeProduto);
 
-void cadastroProdutos()
+        printf("Digite a marca do produto: ");
+        scanf("%s", produtos[produtosCadastrados].marca);
+
+        printf("Insira a quantidade (gramas, ml, etc): ");
+        scanf("%s", produtos[produtosCadastrados].quantidade);
+
+        printf("Digite o preco do produto no super-mercado Shibata: ");
+        scanf("%f", &produtos[produtosCadastrados].preco[0]);
+
+        printf("Digite o preco do produto no super-mercado Semar: ");
+        scanf("%f", &produtos[produtosCadastrados].preco[1]);
+
+        printf("Digite o preco do produto no super-mercado Spani: ");
+        scanf("%f", &produtos[produtosCadastrados].preco[2]);
+        produtosCadastrados++;
+        printf("\n***************************************************\n");
+    }
+    else 
+    {
+        printf("\nLimite de produtos atingido!\n");
+    }
+}
+
+void calculoProdutos(int mercado) 
 {
-	if (produtosCadastrados < 10)
-	{
-		printf("\nDigite o nome do produto: ");
-		scanf("%s", produtos[produtosCadastrados].nomeProduto);
+    if (produtosCadastrados == 0) 
+    {
+        printf("Nao ha nenhum produto cadastrado.\n");
+        return;
+    }
 
-		printf("\nDigite a marca do produto: ");
-		scanf("%s", produtos[produtosCadastrados].marca);
+    int calculo = 0;
+    float menorPreco = FLT_MAX;
 
-		printf("\nInsira a quantidade (gramas, ml, etc): ");
-		scanf("%s", produtos[produtosCadastrados].quantidade);
+    for (int i = 0; i < produtosCadastrados; i++) 
+    {
+        if (produtos[i].preco[mercado] < menorPreco) 
+        {
+            menorPreco = produtos[i].preco[mercado];
+            calculo = i;
+        }
+    }
 
-		printf("\nDigite o preco do produto no super-mercado Shibata: ");
-		scanf("%f", &produtos[produtosCadastrados].preco[0]);
+    printf("\n******** PRODUTO CADASTRADO ********\n");
+    printf("\nProduto: %s ",produtos[calculo].nomeProduto);
+    printf("\nMarca: %s", produtos[calculo].marca);
+    printf("\nQuantidade (kg, g, l): %s", produtos[calculo].quantidade);
+    printf("\nPreco: R$ %.2f\n", produtos[calculo].preco[mercado]);
+    printf("\n************************************\n");
+}
 
-		printf("\nDigite o preco do produto no super-mercado Semar: ");
-		scanf("%f", &produtos[produtosCadastrados].preco[1]);
-
-		printf("\nDigite o preco do produto no super-mercado Spani: ");
-		scanf("%f", &produtos[produtosCadastrados].preco[2]);
-	}
-	else
-	{
-		printf("Limite de produtos atingido.\n");
-	}
-};
-
-int calculoMenorPreco(PerfilProduto produto)
+int main() 
 {
-	int calculo = 0;
+    int menuProdutos;
 
-	for (int i = 0; i < 3; i++)
-	{
-		if (produto.preco[i] < produto.preco[calculo])
-		{
-			calculo = i;
-		};
-	};
+    do 
+    {
+        printf("\nMenu para cadastro de produtos\n");
+        printf("\n***************************************************");
+        printf("\n[1] - Inicializar os dados");
+        printf("\n[2] - Cadastrar um novo produto");
+        printf("\n[3] - Mostrar os produtos mais baratos - Shibata");
+        printf("\n[4] - Mostrar os produtos mais baratos - Semar");
+        printf("\n[5] - Mostrar os produtos mais baratos - Spani");
+        printf("\n[0] - Sair");
+        printf("\n***************************************************\n");
+        printf("Escolha uma opcao de 0 a 5: ");
+        scanf("%d", &menuProdutos);
 
-	return calculo;
-};
+        switch (menuProdutos) 
+        {
+            case 1:
+                printf("\nInicializando os dados. . .\n");
+                inicializacao();
+                break;
+            case 2:
+                cadastroProdutos();
+                break;
+            case 3:
+                calculoProdutos(0);
+                break;
+            case 4:
+                calculoProdutos(1);
+                break;
+            case 5:
+                calculoProdutos(2);
+                break;
+            case 0:
+                printf("Saindo do programa. . .\n");
+                break;
+            default:
+                printf("\nOpcao invalida. Tente novamente!\n\n");
+                break;
+        }
+    } while (menuProdutos != 0);
 
-int main()
-{
-	int menuProdutos;
-
-	do
-	{
-		printf("\nMenu para cadastro de produtos\n");
-		printf("\n********************************************************");
-		printf("\n[1] - Inicializar os dados");
-		printf("\n[2] - Cadastrar um novo produto");
-		printf("\n[3] - Mostrar os produtos mais baratos - Shibata");
-		printf("\n[4] - Mostrar os produtos mais baratos - Semar");
-		printf("\n[5] - Mostrar os produtos mais baratos - Spani");
-		printf("\n[0] - Sair");
-		printf("\n********************************************************\n");
-		printf("\nEscolha uma opcao de 0 a 5: ");
-		scanf("%d", &menuProdutos);
-
-		switch (menuProdutos)
-		{
-			case 1:
-				inicializacao();
-				break;
-			case 2:
-				cadastroProdutos();
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			case 0:
-				printf("Saindo do programa. . .");
-				break;
-			default:
-				printf("\nOpcao invalida. Tente novamente!\n\n");
-				break;
-		}
-	} while (menuProdutos != 0);
-
-};
+    return 0;
+}
